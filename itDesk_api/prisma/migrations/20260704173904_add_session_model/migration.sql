c7bfcd734cedf8fd963e1430a718e487beeff1a1
Adesina Yusuf <adexyuslove@gmail.com>
2026-07-06 20:03:09 -0700

    add session model migration

---
+++ itDesk_api/prisma/migrations/20260704173904_add_session_model/migration.sql
@@ -0,0 +1,15 @@
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "mustChangePassword" BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
